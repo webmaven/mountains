0e19fa6bf68383093d7406434fad34b3ab50ae15
Michael Bernstein <michael@fandomhome.com>
2014-01-12 13:20:27 -0600

    Feature complete.

--- mountains.py
+++ mountains.py
@@ -1,1 +1,37 @@
-#! /usr/bin/python
+#!/usr/bin/env python
+#
+# A simple command-line tool for processing mountain files
+
+import argparse
+import csv
+import cStringIO
+import requests
+from datetime import datetime
+
+timestamp = datetime.now()
+
+
+def unicode_csv_dictreader(unicode_csv_data, **kwargs):
+    """csv.py doesn't do Unicode, so encode as UTF-8"""
+
+    csv_dictreader = csv.DictReader(unicode_csv_data, **kwargs)
+    for row in csv_dictreader:
+        yield dict((key, unicode(value, 'utf-8'))
+                   for key, value in row.iteritems())
+
+if __name__ == '__main__':
+    """Download and parse mountain data files for name and altitude."""
+    parser = argparse.ArgumentParser(description="Parse mountain data files")
+    parser.add_argument("URL", help="A URL to a mountain data file.")
+    args = parser.parse_args()
+    print timestamp.strftime(u"%Y-%m-%d %H:%M:%S (%A)\n")
+    # For potentially large downloads, stream the response, and iterate
+    # over the lines.
+    csvdata = requests.get(args.URL, stream=True).iter_lines()
+    mcsv = unicode_csv_dictreader(csvdata)
+    for row in mcsv:
+        name = row['Name']
+        altitude = row['Altitude (m)']
+        if altitude == 'null':
+            altitude = 'unknown'
+        print u"%s has an altitude of %s meters." % (name, altitude)
